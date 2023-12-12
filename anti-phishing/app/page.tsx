ab03ef603de84a6c47a8c2fca947c368fa64af39
geniefairy <genie@fairytech.ai>
2023-12-12 17:12:09 +0900

    aa

--- anti-phishing/app/page.tsx
+++ anti-phishing/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link href={'/naver'} >Naver</Link>
+      <Link href={'/paypal'} >Paypal</Link>
+      <Link href={'/nhbank'} >NongHyup</Link>
     </main>
   )
 }
