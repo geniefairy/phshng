d5f8ac1dbd9800f781e26614429dbe27cd6231a8
geniefairy <genie@fairytech.ai>
2023-12-12 15:13:12 +0900

    auto build test

---
+++ phishing/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_K_7roM0FiNp33SRTdHMKWjz8SLiVIOM",
+  authDomain: "fairy-anti-phishing.firebaseapp.com",
+  projectId: "fairy-anti-phishing",
+  storageBucket: "fairy-anti-phishing.appspot.com",
+  messagingSenderId: "141922308288",
+  appId: "1:141922308288:web:39aa3765c59b95153ddd25"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
