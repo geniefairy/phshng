9e8825439e2d3fd833a1b099cc9f2bd2b79f8be0
geniefairy <genie@fairytech.ai>
2023-12-13 09:37:33 +0900

    delete out

--- anti-phishing/app/page.tsx
+++ anti-phishing/app/page.tsx
@@ -1,12 +1,28 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { styled } from "styled-components";
 
+const MainPage = styled.div`
+        margin : 0;
+        padding : 0%;
+        width : 100%;
+        display:flex;
+        flex-direction : column;
+        align-items:center;
+        justify-content : center;
+    `
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href={'/naver'} >Naver</Link>
-      <Link href={'/paypal'} >Paypal</Link>
-      <Link href={'/nhbank'} >NongHyup</Link>
-    </main>
+    <MainPage className="flex min-w-screen min-h-screen flex-col items-center justify-center p-24">
+      <div className=''>
+        <Link href={'/naver'} >Naver</Link>
+      </div>
+      <div>
+        <Link href={'/paypal'} >Paypal</Link>
+      </div>
+      <div>
+        <Link href={'/nhbank'} >NongHyup</Link>
+      </div>
+    </MainPage>
   )
 }
--- anti-phishing/next.config.js
+++ anti-phishing/next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output : 'export',
-    images : {
-        loader : 'custom',
-        loaderFile : './app/page.tsx'
+    // output : 'export',
+    // images : {
+    //     loader : 'custom',
+    //     loaderFile : './app/page.tsx'
 
-    }
+    // }
 }
 
 module.exports = nextConfig
